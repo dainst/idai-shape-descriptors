3fb191af84f7b672ac137e4d30715db6d869beab
Michael <michael.kihm@web.de>
2021-03-05 19:31:48 +0100

    Added is image black

---
+++ src/__tests__/isImageBlack.Test.ts
@@ -0,0 +1,12 @@
+import { isImageBlack } from '../image_processing';
+import ndarray = require('ndarray');
+import pool = require('ndarray-scratch');
+
+
+test('isImageBlack false for non black image', () => {
+    expect(isImageBlack( ndarray(new Uint8Array([1, 0, 0, 1]), [2,2]) )).toBe(false);
+});
+
+test('isImageBlack true for black image', () => {
+    expect(isImageBlack( pool.zeros([5,5]) )).toBe(true);
+});
