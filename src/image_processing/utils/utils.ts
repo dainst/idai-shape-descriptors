5487570894a7b2f3371520acff9ed5829da2e262
Michael <michael.kihm@web.de>
2021-03-08 10:54:30 +0100

    JS style for variables

--- src/image_processing/utils/utils.ts
+++ src/image_processing/utils/utils.ts
@@ -13,13 +13,13 @@ import ndarray = require('ndarray');
 export const isImageBlack = (image: ndarray<number>): boolean => {
 
     const [rows, cols] = image.shape;
-    let ret_val = true;
+    let retVal = true;
 
     for (let r = 0; r < rows; r++)
         for(let c = 0; c < cols; c++)
             if(image.get(r,c) !== 0){
-                ret_val = false;
+                retVal = false;
                 break;
             }
-    return ret_val;
+    return retVal;
 };
