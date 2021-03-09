211ffbfdbdaf451b2f22372344c5e6626f202fd5
Michael <michael.kihm@web.de>
2021-03-09 16:15:41 +0100

    Added Points equal function

--- src/__tests__/image_processing/blob_analysis/Points.Test.ts
+++ src/__tests__/image_processing/blob_analysis/Points.Test.ts
@@ -1,8 +1,15 @@
-import { Point, addPoints } from '../../../image_processing/blob_analysis/point';
+import { Point, addPoints, isEqualPoints } from '../../../image_processing/blob_analysis/point';
 
 test('Should add points with addPoints', () => {
     const p1: Point = { x: 2, y: 4 };
     const p2: Point = { x: 5, y: 8 };
     const expected_out: Point = { x: 7, y: 12 };
     expect(addPoints(p1,p2)).toEqual(expected_out);
+});
+
+
+test('Should be able to compare to Points at different memory locations', () => {
+    const p1: Point = { x: 2, y: 4 };
+    const p2: Point = { x: 5, y: 8 };
+    expect( isEqualPoints(p1,p2)).toBe(false);
 });
--- src/image_processing/blob_analysis/point.ts
+++ src/image_processing/blob_analysis/point.ts
@@ -7,3 +7,6 @@ export const addPoints = (p1: Point, p2: Point): Point => {
 
     return { x: p1.x + p2.x, y: p1.y + p2.y };
 };
+
+export const isEqualPoints = (p1: Point, p2: Point): boolean =>
+    Object.entries(p1).toString() === Object.entries(p2).toString();
