5130248b64a83318831811eac5d139771021a021
Michael <michael.kihm@web.de>
2021-03-10 17:05:39 +0100

    Added contourLength

---
+++ src/__tests__/image_processing/blob_analysis/contourLength.Test.ts
@@ -0,0 +1,28 @@
+import { Contour, contourLength } from '../../../image_processing/blob_analysis/contour';
+import { Point } from '../../../image_processing/blob_analysis/point';
+
+
+describe ('Test contourLength', () => {
+    const contour = new Contour();
+    const points: Point[] = [
+        { y: 1, x: 0 },
+        { y: 2, x: 1 },
+        { y: 2, x: 2 },
+        { y: 1, x: 3 },
+        { y: 0, x: 3 },
+        { y: 0, x: 2 },
+        { y: 0, x: 1 },
+    ];
+    const sq2 = Math.sqrt(2);
+    const expected_perimeter = (sq2 + 1 + sq2 + 1 + 1 + 1 + sq2) * 0.95;
+
+    beforeAll(() => {
+        for( const point of points)
+            contour.addPoint(point);
+    });
+
+    it('Should calculated expected perimeter', () => {
+        expect(contourLength(contour)).toBe(expected_perimeter);
+    });
+
+});
--- src/image_processing/blob_analysis/contour.ts
+++ src/image_processing/blob_analysis/contour.ts
@@ -6,3 +6,17 @@ export class Contour {
         this.points.push(point);
     };
 }
+
+
+export const contourLength = (contour:Contour): number => {
+    let perimeter = 0;
+    const cnt_points = contour.points.length;
+    
+    for(let i = 0; i < cnt_points; i++)
+        if( contour.points[i % cnt_points].x !== contour.points[(i+1) % cnt_points].x &&
+            contour.points[i % cnt_points].y !== contour.points[(i+1) % cnt_points].y)
+            perimeter += Math.sqrt(2);
+        else
+            perimeter += 1;
+    return perimeter * 0.95;
+};
