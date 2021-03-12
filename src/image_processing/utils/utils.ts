cc61d03f0f19b4bc9d229c40336048d8669c2a55
Michael <michael.kihm@web.de>
2021-03-12 10:01:58 +0100

    Added tensorToBinaryImage

--- src/image_processing/utils/utils.ts
+++ src/image_processing/utils/utils.ts
@@ -4,6 +4,10 @@
  * Includes image processing util functions
  */
 import ndarray = require('ndarray');
+import pool from 'ndarray-scratch';
+import { FOREGROUND } from '../blob_analysis/constants';
+import * as tf from '@tensorflow/tfjs';
+import pack = require('ndarray-pack');
 
 /**
  * Check if image is black
@@ -22,4 +26,26 @@ export const isImageBlack = (image: ndarray<number>): boolean => {
                 break;
             }
     return retVal;
+};
+
+
+/**
+ * Converts Tf Tensor binary class image
+ * (one channel image with class labels 0 or 1 as pixel values)
+ * @param {tf.Tensor} tensor - Tensor as class image
+ * @returns {ndarray<number>} Binary image as type ndarray
+ */
+export const tensorToBinaryImage = (tensor: tf.Tensor ): ndarray<number> => {
+    
+    const [rows, cols] = tensor.shape;
+    const data = tensor.arraySync();
+    const binaryImage = pack(data);
+    const image = pool.zeros(tensor.shape);
+
+    for(let r = 0; r < rows; r++)
+        for(let c = 0; c < cols; c++)
+            if(binaryImage.get(r,c) === FOREGROUND)
+                image.set(r,c,FOREGROUND);
+
+    return image;
 };
