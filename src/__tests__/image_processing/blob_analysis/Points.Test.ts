432d36f6e4e2ca2e96acdbc80d317bfeff6032e5
Michael <michael.kihm@web.de>
2021-03-17 15:27:36 +0100

    Added point functionalities

--- src/__tests__/image_processing/blob_analysis/Points.Test.ts
+++ src/__tests__/image_processing/blob_analysis/Points.Test.ts
@@ -1,4 +1,9 @@
-import { Point, addPoints, isEqualPoints } from '../../../image_processing/blob_analysis/point';
+import {
+    Point,
+    addPoints,
+    isEqualPoints,
+    toArray,
+    subtractPoints } from '../../../image_processing/blob_analysis/point';
 
 test('Should add points with addPoints', () => {
     const p1: Point = { x: 2, y: 4 };
@@ -14,4 +19,22 @@ test('Should be able to compare to Points at different memory locations', () => 
     const p3: Point = { y: 8, x: 5 };
     expect( isEqualPoints(p1,p2)).toBe(false);
     expect( isEqualPoints(p2,p3)).toBe(true);
+});
+
+
+test('Should convert Point to array', () => {
+    const [x ,y] = [Math.floor(Math.random() * 40), Math.floor(Math.random() * 100)];
+    const p1: Point = { x, y };
+    const expected_out = [x, y];
+    expect(toArray(p1)).toEqual(expected_out);
+});
+
+
+test('Should be able to subtract points', () => {
+    const [x1 ,y1] = [Math.floor(Math.random() * 40), Math.floor(Math.random() * 100)];
+    const [x2 ,y2] = [Math.floor(Math.random() * 40), Math.floor(Math.random() * 100)];
+    const p1: Point = { x: x1, y: y1 };
+    const p2: Point = { x: x2, y: y2 };
+    const expected_point: Point = { x: x1 - x2, y: y1 - y2 };
+    expect(subtractPoints(p1,p2)).toEqual(expected_point);
 });
--- src/image_processing/blob_analysis/point.ts
+++ src/image_processing/blob_analysis/point.ts
@@ -8,5 +8,9 @@ export const addPoints = (p1: Point, p2: Point): Point => {
     return { x: p1.x + p2.x, y: p1.y + p2.y };
 };
 
+export const subtractPoints = (p1: Point, p2: Point): Point => {return { x: p1.x - p2.x, y: p1.y - p2.y }; };
+
+export const toArray = (p: Point): number[] => [p.x, p.y];
+
 export const isEqualPoints = (p1: Point, p2: Point): boolean =>
     p1.x === p2.x && p1.y === p2.y;
